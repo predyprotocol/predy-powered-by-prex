0d5b06ce6fe9ef9e651ee054914c64258a9314ea
syuhei176 <syuhei176@gmail.com>
2024-10-16 12:27:50 +0900

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      injectRegister: 'auto',
-      manifest: {
-        name: 'Prex Wallet',
-        short_name: 'PrexWallet',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/icon192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
   ],
   build: {
     rollupOptions: {
