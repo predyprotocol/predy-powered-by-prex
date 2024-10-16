e8ff4d1fd177c0eba04abab6d55274f41b967d44
syuhei176 <syuhei176@gmail.com>
2024-10-16 13:25:11 +0900

    update github repo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ export const Footer = () => {
         <path d="M13.54 12a6.8 6.8 0 01-6.77 6.82A6.8 6.8 0 010 12a6.8 6.8 0 016.77-6.82A6.8 6.8 0 0113.54 12zM20.96 12c0 3.54-1.51 6.42-3.38 6.42-1.87 0-3.39-2.88-3.39-6.42s1.52-6.42 3.39-6.42 3.38 2.88 3.38 6.42M24 12c0 3.17-.53 5.75-1.19  5.75-.66 0-1.19-2.58-1.19-5.75s.53-5.75 1.19-5.75C23.47 6.25 24 8.83 24 12z" />
       </svg>
     </a>
-    <a href="https://github.com/predyprotocol" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-300">
+    <a href="https://github.com/predyprotocol/predy-powered-by-prex" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-300">
       <span className="sr-only">GitHub</span>
       <Github className="h-5 w-5" />
     </a>
